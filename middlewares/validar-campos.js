7464c5d456ea14dc7916d65970cf316f2e5363aa
kanondrago <ronaldchavezr@gmail.com>
2024-05-06 11:22:41 +0200

    Middleware personalizado

---
+++ middlewares/validar-campos.js
@@ -0,0 +1,22 @@
+
+const { response } = require('express');
+const { validationResult } = require('express-validator');
+
+const validarCampos = (req, res = response, next) => {
+    
+    // se crea un arreglo de errores
+    const errores = validationResult(req)
+
+    if(!errores.isEmpty()) {
+        return res.status(400).json({
+        ok: false,
+        errors: errores.mapped() 
+        })
+    }
+
+    next()
+}
+
+module.exports = {
+    validarCampos
+};
