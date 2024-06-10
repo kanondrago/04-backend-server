1a66057d728649342bea53c6a3a7a32b754e8871
kanondrago <ronaldchavezr@gmail.com>
2024-06-10 09:01:47 +0200

    Implementando un switch

--- routes/busquedas.routes.js
+++ routes/busquedas.routes.js
@@ -9,7 +9,7 @@ const { login } = require('../controllers/auth.controllers');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { busquedaTodo }  = require('../controllers/busqueda.controllers')
+const { busquedaTodo, busquedaDocumentosColeccion }  = require('../controllers/busqueda.controllers')
 
 
 const router = Router();
@@ -19,5 +19,8 @@ router.get('/:busqueda', [
     validarJWT
 ], busquedaTodo)
 
+router.get('/coleccion/:tabla/:busqueda', [
+    validarJWT
+], busquedaDocumentosColeccion)
 
 module.exports = router;
